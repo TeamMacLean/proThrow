054940cd33df0c7239ff0d66a779c664bfffa666
Martin Page <wookoouk@gmail.com>
2016-04-05 11:54:06 +0100

    from neil

--- public/js/main.js
+++ public/js/main.js
@@ -5,11 +5,65 @@ var className = 'disabled';
 if (form && requiredCheckbox) {
   addTheListener();
   checkCheckBox();
+  checkCompletion();
+
+  sampleManager();
+
 }
 
-//TODO check if on admin show page
 updateCompleteStatus();
 addNote();
+
+function checkCompletion() {
+
+  var groupSelect = ':input[required]:visible, select[required]:visible';
+
+  $('.group').each(function () {
+    var $group = $(this);
+    //console.log('group');
+    $group.find(groupSelect).each(function () {
+      //console.log('input');
+      var $input = $(this);
+      $input.on('input', function () {
+        //console.log('change', $input.val());
+
+        var $badge = $group.find('.badge');
+
+        if (groupValid($group)) {
+          console.log("valid");
+          if (!$badge.hasClass('complete')) {
+            $badge.addClass('complete');
+          }
+        } else {
+          console.log("not valid");
+          if ($badge.hasClass('complete')) {
+            $badge.removeClass('complete');
+          }
+        }
+      })
+    })
+  });
+  function groupValid(g) {
+    var allvalid = true;
+    g.find(groupSelect).each(function () {
+
+      if ($(this).is('input:text') || $(this).is(':input[type="number"]')) {
+        if (!$(this).val()) {
+          allvalid = false;
+        }
+      } else if ($(this).is('select')) {
+        var text = $(this).find(':selected').text();
+        if (text.length < 1) {
+          allvalid = false;
+        }
+      }
+
+
+    });
+    return allvalid;
+  }
+}
+
 
 function addTheListener() {
   requiredCheckbox.on('change', checkCheckBox);
@@ -18,41 +72,21 @@ function addTheListener() {
 function checkCheckBox() {
   if (requiredCheckbox.is(':checked')) {
     form.removeClass('disabled');
-
     $('html, body').animate({
       scrollTop: form.offset().top
     }, 1000);
-
   } else {
     form.addClass('disabled');
   }
 }
 
-
-//function addClass(el) {
-//  if (el.classList)
-//    el.classList.add(className);
-//  else
-//    el.className += ' ' + className;
-//}
-
-//function removeClass(el) {
-//  if (el.classList)
-//    el.classList.remove(className);
-//  else
-//    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-//}
-
 function updateCompleteStatus() {
   var toggle = $('#complete-toggle');
   var url = '/admin/request/' + toggle.data('uuid') + '/toggle';
   toggle.on('change', function (e) {
-    //console.log('toggle', e);
-
     $.ajax({
       url: url
     });
-
   })
 }
 
@@ -60,11 +94,8 @@ function addNote() {
   var noteText = $('#note-text');
   var noteButton = $('#note-button');
   var notes = $('#notes');
-
   var url = '/admin/request/' + noteButton.data('uuid') + '/addnote';
-
   noteButton.on('click', function (e) {
-
     $.ajax({
       type: "POST",
       url: url,
@@ -73,6 +104,37 @@ function addNote() {
         notes.find('ul').append('<li>' + noteText.val() + '</li>')
       }
     });
-
   })
+}
+
+function sampleManager() {
+
+  function addSample() {
+    var aSample = `<div class="dragg">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Sample number <span data-icon="&#x74;" class="tip"
+                                                           data-tipso="This needs to be filled out"></span></label>
+                                <input class="form-control" type="number" min="0" max="150" id="sampleNumber" name="sampleNumber" required>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Sample description <span data-icon="&#x74;" class="tip"
+                                                                data-tipso="This needs to be filled out"></span></label>
+                                <input class="form-control" type="text" id="sampleDescription" name="sampleDescription" required>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+    $('#samples').append(aSample);
+  }
+
+  addSample();
+
+  $('#addSample').on('click', function () {
+    addSample();
+  })
+
 }
