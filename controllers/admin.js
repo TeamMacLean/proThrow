23f4741e8237ad53067163016854256a0e74ec90
George Deeks <georgedeeks@gmail.com>
2024-02-08 11:59:02 +0000

    feat: discarded

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,34 +1,37 @@
-const Request = require('../models/request');
-const renderError = require('../lib/renderError');
+const Request = require("../models/request");
+const renderError = require("../lib/renderError");
 const admin = {};
 
 admin.index = (req, res) => {
+  Request.run()
+    .then((requests) => {
+      const completedRequests = [];
+      const incompleteRequests = [];
+      const discardedRequests = [];
 
-    Request
-        .run()
-        .then(requests => {
-            const completedRequests = [];
-            const incompleteRequests = [];
+      requests.sort(function (a, b) {
+        return new Date(b.createdAt) - new Date(a.createdAt);
+      });
 
-            requests.sort(function (a, b) {
-                return new Date(b.createdAt) - new Date(a.createdAt);
-            });
+      requests.map((m) => {
+        if (m.status === "complete") {
+          completedRequests.push(m);
+        } else if (m.status === "discarded") {
+          discardedRequests.push(m);
+        } else {
+          incompleteRequests.push(m);
+        }
+      });
 
-            requests.map(m => {
-                if (m.status === 'complete') {
-                    completedRequests.push(m);
-                } else {
-                    incompleteRequests.push(m);
-                }
-            });
-
-            return res.render('admin/index', {
-                // requests,
-                completedRequests,
-                incompleteRequests
-            });
-        }).catch(err => {
-        return renderError(err, res);
+      return res.render("admin/index", {
+        // requests,
+        completedRequests,
+        incompleteRequests,
+        discardedRequests,
+      });
+    })
+    .catch((err) => {
+      return renderError(err, res);
     });
 };
 
