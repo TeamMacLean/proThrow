860ba409f9d19e3a09a296db0d8b3b4412be064d
George Deeks <georgedeeks@gmail.com>
2024-05-31 15:36:00 +0100

    lint

--- models/construct.js
+++ models/construct.js
@@ -1,27 +1,20 @@
-const thinky = require('../lib/thinky.js');
+const thinky = require("../lib/thinky.js");
 const type = thinky.type;
-const config = require('../config.js');
-const Construct = thinky.createModel('Construct', {
-    id: type.string(),
-    requestID: type.string(),
-    accession: type.string().required(),
-    sequenceInfo: type.string().required(),
-    dbEntry: type.string().required()
-    // uid: type.string().required(),
-    // requestID: type.string(),
-    // name: type.string().required(),
-    // path: type.string().required(),
-    // description: type.string()
+const config = require("../config.js");
+const Construct = thinky.createModel("Construct", {
+  id: type.string(),
+  requestID: type.string(),
+  accession: type.string().required(),
+  sequenceInfo: type.string().required(),
+  dbEntry: type.string().required(),
+  // uid: type.string().required(),
+  // requestID: type.string(),
+  // name: type.string().required(),
+  // path: type.string().required(),
+  // description: type.string()
 });
 
-
-// SampleImage.define('getURL', function () {
-//     return config.supportingImageRootURL + this.uid;
-// });
-// SampleImage.define('getPreviewURL', function () {
-//     return config.supportingImagePreviewRootURL + this.uid;
-// });
 module.exports = Construct;
 
-const Request = require('./request');
-Construct.belongsTo(Request, 'request', 'requestID', 'id');
+const Request = require("./request");
+Construct.belongsTo(Request, "request", "requestID", "id");
--- models/sampleImage.js
+++ models/sampleImage.js
@@ -1,23 +1,22 @@
-const thinky = require('../lib/thinky.js');
+const thinky = require("../lib/thinky.js");
 const type = thinky.type;
-const config = require('../config.js');
-const SampleImage = thinky.createModel('SampleImage', {
-    id: type.string(),
-    uid: type.string().required(),
-    requestID: type.string(),
-    name: type.string().required(),
-    path: type.string().required(),
-    description: type.string()
+const config = require("../config.js");
+const SampleImage = thinky.createModel("SampleImage", {
+  id: type.string(),
+  uid: type.string().required(),
+  requestID: type.string(),
+  name: type.string().required(),
+  path: type.string().required(),
+  description: type.string(),
 });
 
-
-SampleImage.define('getURL', function () {
-    return config.supportingImageRootURL + this.uid;
+SampleImage.define("getURL", function () {
+  return config.supportingImageRootURL + this.uid;
 });
-SampleImage.define('getPreviewURL', function () {
-    return config.supportingImagePreviewRootURL + this.uid;
+SampleImage.define("getPreviewURL", function () {
+  return config.supportingImagePreviewRootURL + this.uid;
 });
 module.exports = SampleImage;
 
-const Request = require('./request');
-SampleImage.belongsTo(Request, 'request', 'requestID', 'id');
+const Request = require("./request");
+SampleImage.belongsTo(Request, "request", "requestID", "id");
